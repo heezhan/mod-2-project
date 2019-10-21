2d8d031ecf7ac2e6e054b344f5abbd3bca642228
David Fellows <dfellows12@gmail.com>
2019-10-21 14:32:10 -0400

    Controller actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,31 @@
 class UsersController < ApplicationController
+    def index
+        @users = User.all
+    end
+
+    def show
+        @user = 
+    end
+
+    def new
+    end
+
+    def create
+    end
+
+    def edit
+    end
+
+    def update
+    end
+
+    def destroy
+    end
+
+    private
+
+    def user_params
+        
+    end
+
 end
