2db770722d1f79587e9da7ba592ebd28b9593cae
heezhan <hhan1194@gmail.com>
2019-10-21 16:39:47 -0400

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :restaurants, only: [:index, :show]
   resources :reviews, only: [:index, :show]
+  get '/', to: 'user#home', as: "home"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
