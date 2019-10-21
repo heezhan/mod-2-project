b4a13111932c87c64a7f68965663aec6e67f87f4
David Fellows <dfellows12@gmail.com>
2019-10-21 13:12:52 -0400

    Created models, controllers

---
+++ db/migrate/20191021171037_create_restaurants.rb
@@ -0,0 +1,14 @@
+class CreateRestaurants < ActiveRecord::Migration[6.0]
+  def change
+    create_table :restaurants do |t|
+      t.string :address
+      t.integer :price
+      t.string :category
+      t.integer :phone_number
+      t.string :website_url
+      t.string :hours_of_operation
+
+      t.timestamps
+    end
+  end
+end
