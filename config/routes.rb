cbd78f0c0947799303fefd893821ed10758c4d67
David Fellows <dfellows12@gmail.com>
2019-10-22 15:37:00 -0400

    fix

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   resources :users
   resources :reservations
   resources :locations, only: [:index, :show]
-  get '/', to: 'user#home', as: "home"
+  get '/', to: 'users#home', as: "home"
   get '/login', to: 'sessions#login'
   post '/login', to: 'sessions#process_login'
   get '/logout', to: 'sessions#logout'
