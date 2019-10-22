010ab6f5be4bdefd32d4bd06e13e5ed46447d463
heezhan <hhan1194@gmail.com>
2019-10-22 14:05:39 -0400

    changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_22_135651) do
+ActiveRecord::Schema.define(version: 2019_10_22_151926) do
+
+  create_table "locations", force: :cascade do |t|
+    t.string "region"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
